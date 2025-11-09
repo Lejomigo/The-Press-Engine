1d62fd02127841553129861bbb1fb4af7d85ba52
Lejomigo <leopoldomicettgonzalez@gmail.com>
2025-11-09 13:22:22 -0400

    modelos creados

--- TPE/src/main/java/com/software/TPE/model/Post.java
+++ TPE/src/main/java/com/software/TPE/model/Post.java
@@ -1,17 +1,24 @@
 package com.software.TPE.model;
 
+import lombok.Generated;
+
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 
-public class New {
-    private String id;
+public class Post {
+    @Id
+    private UUID id;
     private String title;
     private String content;
     private Boolean state;
-    private String imagenUrl;
+    private String category;
+    private String imageUrl;
+
 
 }
